008799fb45d483d33edc40a4a6375171a45915f9
Kukumberchick <durovalexei93@gmail.com>
2024-03-07 15:15:19 +0600

    new project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import "./App.scss"
+import Home from "./home/Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
---
+++ src/home/Home.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+    return (
+        <div>
+            <div className="head">
+                <center>
+                    <h2>Hello World</h2>
+                    <button>Click</button>
+                </center>
+
+            </div>
+        </div>
+    )
+}
